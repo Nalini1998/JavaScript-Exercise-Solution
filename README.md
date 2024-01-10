7db89b0935fe0bc41b82202603c55c5ca3fa4be0
Nalini Vo <131301452+Nalini1998@users.noreply.github.com>
2024-01-10 22:50:33 +0700

    Files uploaded

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# JavaScript-Exercise-Solution
+# JS Solution 2
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Nalini-Vo-the-solid/pen/abMmONg](https://codepen.io/Nalini-Vo-the-solid/pen/abMmONg).
+
---
+++ script.js
@@ -0,0 +1,3 @@
+const myFunc = () => {window.print()};
+// document.getElementById("demo1").innerHTML = window.print();
+//document.getElementById("demo2").innerHTML = `Current time is : ${hour} ${pre} : ${min} : ${sec}`;
