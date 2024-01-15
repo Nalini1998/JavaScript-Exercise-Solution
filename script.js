c70175530b169f04f36688a8c112ad2871baeed4
Nalini Vo <131301452+Nalini1998@users.noreply.github.com>
2024-01-15 23:41:26 +0700

    Files uploaded

---
+++ script.js
@@ -0,0 +1,11 @@
+const division = () => {
+  const num1 = document.getElementById('num1').value;
+  const num2 = document.getElementById('num2').value;
+  document.getElementById('output').innerHTML = num1/num2;
+}
+
+const multiplication = () => {
+  const num1 = document.getElementById('num1').value;
+  const num2 = document.getElementById('num2').value;
+  document.getElementById('output').innerHTML = num1*num2;
+}
